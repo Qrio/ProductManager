6c714067d5bf21978ce7670a0b9b0b728bcd4f0e
bbatan <bbatan@hotmail.com>
2014-02-11 01:32:25 -0800

    Purchase Manager Initial commit

---
+++ ProjectX_Proto1/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjectX_Proto2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ThemeManager.ApplyTheme( Application.Current, "Blue");
+        }
+
+        private void loginPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        
+    }
+
+    public static class ThemeManager 
+    {
+        public static void ApplyTheme(this Application app, string theme)
+        {
+            ResourceDictionary dictionary = GetThemeResourceDictionary(theme);
+
+            if (dictionary != null)
+            {
+                app.Resources.MergedDictionaries.Clear();
+                app.Resources.MergedDictionaries.Add(dictionary);
+            }
+        }
+
+        public static void ApplyTheme(this ContentControl control, string theme)
+        {
+            ResourceDictionary dictionary = GetThemeResourceDictionary(theme);
+
+            if (dictionary != null)
+            {
+                control.Resources.MergedDictionaries.Clear();
+                control.Resources.MergedDictionaries.Add(dictionary);
+            }
+        }
+
+        public static ResourceDictionary GetThemeResourceDictionary(string theme)
+        {
+            if (theme != null)
+            {
+                // Assembly assembly = Assembly.LoadFrom("WPF.Themes.dll");
+                string packUri = String.Format(@"/Themes/{0}.xaml", theme);
+                return Application.LoadComponent(new Uri(packUri, UriKind.Relative)) as ResourceDictionary;
+            }
+            return null;
+        }
+    }
+}
