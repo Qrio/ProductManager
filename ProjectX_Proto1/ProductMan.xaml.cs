1a85b77108cabc325122c988716e6368eac25174
bbatan <bbatan@hotmail.com>
2014-02-13 22:54:17 -0800

    Updated #FontAwesome for the icons visual style

--- ProjectX_Proto1/ProductMan.xaml.cs
+++ ProjectX_Proto1/ProductMan.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Controls.Primitives;
 using System.Windows.Shapes;
 
 namespace ProjectX_Proto2
@@ -17,11 +18,31 @@ namespace ProjectX_Proto2
     /// <summary>
     /// Interaction logic for ProductMan.xaml
     /// </summary>
+    /// 
+    
+    
     public partial class ProductMan : Window
     {
+        private Popup _MyPopup;
+
         public ProductMan()
         {
             InitializeComponent();
+            _MyPopup = Resources["myPopup"] as Popup;
         }
+
+        private void pnlProfile_Click(object sender, MouseButtonEventArgs e)
+        {
+            if (_MyPopup.IsOpen == true)
+            {
+                _MyPopup.IsOpen = false;
+            }
+            else
+            {
+                _MyPopup.IsOpen = true;
+            }
+            
+        }
+
     }
 }
